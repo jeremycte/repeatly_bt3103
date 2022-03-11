5039f42fbeacdafa3c4796de03c0acd554801b68
clvnthe <clvnthe@hotmail.com>
2022-03-11 14:04:28 +0800

    Completed login,register,reset password

---
+++ src/views/resetPassword.vue
@@ -0,0 +1,16 @@
+<template>
+  <h3> This is the reset password page</h3>
+  <forgetPassword></forgetPassword>
+</template>
+
+<script>
+import forgetPassword from "@/components/forgetPassword";
+export default {
+  name: "resetPassword",
+  components:{forgetPassword},
+}
+</script>
+
+<style scoped>
+
+</style>

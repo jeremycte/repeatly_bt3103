5d12305c34ee2e59211e8f2749732c24e9767794
brendanlyc <brendanlyc@gmail.com>
2022-03-30 02:18:17 +0800

    Student study deck stats page

---
+++ src/views/StudentStudyDeckStats.vue
@@ -0,0 +1,391 @@
+<template>
+  <div class="container-center-horizontal-studentstats">
+    <div class="student-study-deck-stats-studentstats screen">
+      <div class="overlap-group2-studentstats">
+        <div class="background-studentstats"></div>
+        <div class="overlap-group-studentstats">
+          <img class="answer-box-studentstats" src="https://anima-uploads.s3.amazonaws.com/projects/6231a346e960d27abb0c8fdd/releases/6237f289ec7f94c8a6fb2210/img/answer-box@1x.png" />
+          <img class="question-studentstats" src="https://anima-uploads.s3.amazonaws.com/projects/6231a346e960d27abb0c8fdd/releases/6237f289ec7f94c8a6fb2210/img/question@1x.png" />
+        </div>
+        <div class="header-studentstats">
+          <h1 class="title-studentstats inter-bold-heavy-metal-40px">Study Statistics</h1>
+        </div>
+        <div class="cards-studied-3-cards-studentstats inter-bold-white-36px">Cards Studied: 3 cards</div>
+        <div class="cards-left-8-cards-studentstats inter-semi-bold-white-28px">Cards Left: 8 cards</div>
+        <div class="dark-theme-studentstats">
+          <div class="frame-1-studentstats">
+            <div class="return-to-deck-studentstats inter-semi-bold-white-28px">Return to Deck</div>
+          </div>
+        </div>
+        <div class="overlap-group1-studentstats">
+          <div class="header-1-studentstats">
+            <div class="logo-studentstats">
+                <img class="logo-icon-1-studentstats" src="https://anima-uploads.s3.amazonaws.com/projects/6231a346e960d27abb0c8fdd/releases/6231bb3bec371df0e72ad002/img/logo-icon-1@2x.png"/>
+                <div class="repeatly-studentstats inter-bold-white-30px">Repeatly</div>
+            </div>
+          </div>
+          <div class="body-studentstats">
+            <div class="overlap-group-1-studentstats">
+                <div class="homepage-studentstats">
+                    <img class="icon-home-studentstats" src="https://anima-uploads.s3.amazonaws.com/projects/6231a346e960d27abb0c8fdd/releases/6237f289ec7f94c8a6fb2210/img/mdi-home-circle-1@2x.png"/>
+                    <div class="homepage-1-studentstats inter-semi-bold-white-20px">Homepage</div>
+                </div>
+            </div>
+            <div class="groups-studentstats">
+                <img class="groups-1-studentstats" src="https://anima-uploads.s3.amazonaws.com/projects/6231a346e960d27abb0c8fdd/releases/6237f289ec7f94c8a6fb2210/img/groups@2x.png"/>
+                <div class="groups-2-studentstats inter-semi-bold-white-20px">Groups</div>
+            </div>
+            <div class="settings-studentstats">
+                <img class="icon-settings-studentstats" src="https://anima-uploads.s3.amazonaws.com/projects/6231a346e960d27abb0c8fdd/releases/6237f289ec7f94c8a6fb2210/img/ci-settings-filled-1@2x.png"/>
+                <div class="settings-1-studentstats inter-semi-bold-white-20px">Settings</div>
+            </div>
+          </div>
+          <div class="footer-studentstats">
+            <img class="line-4-studentstats" src="https://anima-uploads.s3.amazonaws.com/projects/6231a346e960d27abb0c8fdd/releases/6237f289ec7f94c8a6fb2210/img/line-4@2x.png"/>
+            <div class="username-studentstats">
+                <img class="oval-studentstats" src="https://anima-uploads.s3.amazonaws.com/projects/6231a346e960d27abb0c8fdd/releases/6231bb3bec371df0e72ad002/img/oval-1@2x.png"/>
+                <div class="name-studentstats inter-semi-bold-white-20px">Jeremy</div>
+            </div>
+            <div class="sync-now-studentstats">
+                <img class="sync-arrows-1-1-studentstats" src="https://anima-uploads.s3.amazonaws.com/projects/6231a346e960d27abb0c8fdd/releases/6231bb3bec371df0e72ad002/img/sync-arrows--1--1@2x.png"/>
+                <div class="sync-now-1-studentstats inter-semi-bold-white-20px">Sync Now</div>
+            </div>
+            <div class="night-mode-toggle">
+                <img class="toggle" src="https://anima-uploads.s3.amazonaws.com/projects/6231a346e960d27abb0c8fdd/releases/6237f289ec7f94c8a6fb2210/img/toggle@2x.png"/>
+                <div class="dark-mode valign-text-middle inter-semi-bold-white-20px">Dark Mode</div>
+            </div>
+          </div>
+        </div>
+        <img class="bxhelp-circle-studentstats" src="https://anima-uploads.s3.amazonaws.com/projects/6231a346e960d27abb0c8fdd/releases/6237f289ec7f94c8a6fb2210/img/bx-help-circle-6@2x.png" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "StudentStudyDeckStats",
+};
+</script>
+
+<style>
+.student-study-deck-stats-studentstats {
+  align-items: flex-start;
+  background-color: var(--white);
+  display: flex;
+  height: 1024px;
+  overflow: hidden;
+  width: 1440px;
+}
+
+.overlap-group2-studentstats {
+  height: 1210px;
+  margin-left: -16px;
+  position: relative;
+  width: 1476px;
+}
+
+.background-studentstats {
+  background-color: var(--white);
+  box-shadow: 0px 2px 8px #00000014;
+  height: 1026px;
+  left: 35px;
+  position: absolute;
+  top: 185px;
+  width: 1442px;
+}
+
+.overlap-group-studentstats {
+  background-color: var(--humming-bird);
+  border-radius: 40px;
+  box-shadow: 0px 2px 8px #00000022;
+  height: 858px;
+  left: 341px;
+  position: absolute;
+  top: 126px;
+  width: 1072px;
+}
+
+.answer-box-studentstats {
+  height: 200px;
+  left: 250px;
+  position: absolute;
+  top: 505px;
+  width: 546px;
+}
+
+.question-studentstats {
+  height: 488px;
+  left: 250px;
+  position: absolute;
+  top: 23px;
+  width: 546px;
+}
+
+.header-studentstats {
+  display: flex;
+  height: 112px;
+  left: 336px;
+  position: absolute;
+  top: 38px;
+  width: 314px;
+}
+
+.title-studentstats {
+  height: 112px;
+  letter-spacing: 0;
+  line-height: 56px;
+  width: 312px;
+}
+
+.cards-studied-3-cards-studentstats {
+  left: 678px;
+  letter-spacing: 0;
+  line-height: 50.4px;
+  position: absolute;
+  text-align: center;
+  text-decoration: underline;
+  top: 177px;
+  width: 386px;
+}
+
+.cards-left-8-cards-studentstats {
+  left: 678px;
+  letter-spacing: 0;
+  line-height: 39.2px;
+  position: absolute;
+  text-align: center;
+  text-decoration: underline;
+  top: 530px;
+  width: 386px;
+}
+
+.dark-theme-studentstats {
+  background-color: var(--shamrock);
+  border-radius: 30px;
+  cursor: pointer;
+  display: flex;
+  height: 74px;
+  left: 623px;
+  position: absolute;
+  top: 854px;
+  width: 495px;
+}
+
+.frame-1-studentstats {
+  border-radius: 14px;
+  display: flex;
+  height: 30.62px;
+  margin-left: 125.5px;
+  margin-top: 21.7px;
+  width: 246.46px;
+}
+
+.return-to-deck-studentstats {
+  height: 34px;
+  letter-spacing: 0;
+  margin-left: 22.7px;
+  margin-top: -1.7px;
+  width: 201px;
+}
+
+.overlap-group1-studentstats {
+  align-items: flex-end;
+  background-color: var(--dull-lavender);
+  border-radius: 20px;
+  box-shadow: 0px 2px 10px #0000002c;
+  display: flex;
+  flex-direction: column;
+  left: -1px;
+  min-height: 1026px;
+  padding: 23px 20px;
+  position: absolute;
+  top: -1px;
+  width: 297px;
+}
+
+.bxhelp-circle-studentstats {
+  height: 79px;
+  left: 1366px;
+  position: absolute;
+  top: 936px;
+  width: 79px;
+}
+
+.header-1-studentstats {
+  align-self: center;
+  display: flex;
+  margin-left: 6.54px;
+  margin-top: 16px;
+  width: 222px;
+}
+
+.logo-studentstats {
+  display: flex;
+  width: 223.58px;
+}
+
+.logo-icon-1-studentstats {
+  height: 58px;
+  margin-left: -0.3px;
+  object-fit: cover;
+  width: 56.88px;
+}
+
+.repeatly-studentstats {
+  height: 50.75px;
+  letter-spacing: 0;
+  line-height: 42px;
+  margin-left: 11px;
+  margin-top: 2.4px;
+  width: 154.04px;
+}
+
+.footer-studentstats {
+  display: flex;
+  flex-direction: column;
+  height: 200px;
+  margin-top: 433px;
+  position: relative;
+  width: 248px;
+}
+
+.line-4-studentstats {
+  height: 1px;
+  margin-top: -0.5px;
+  width: 248px;
+}
+
+.username-studentstats {
+  align-items: flex-start;
+  display: flex;
+  height: 40px;
+  margin-left: 12px;
+  margin-top: 18.5px;
+  min-width: 212px;
+}
+
+.oval-studentstats {
+  height: 52px;
+  margin-left: -7px;
+  margin-top: -5px;
+  object-fit: cover;
+  width: 52px;
+}
+
+.name-studentstats {
+  align-self: center;
+  letter-spacing: 0;
+  margin-left: 14px;
+  min-height: 24px;
+  width: 72px;
+}
+
+.sync-now-studentstats {
+  display: flex;
+  height: 32.64px;
+  margin-left: 18px;
+  margin-top: 39px;
+  width: 152px;
+}
+
+.sync-arrows-1-1-studentstats {
+  height: 32.64px;
+  margin-left: 0;
+  margin-top: -0.2px;
+  object-fit: cover;
+  width: 28.44px;
+}
+
+.sync-now-1-studentstats {
+  height: 24px;
+  letter-spacing: 0;
+  margin-left: 24.6px;
+  margin-top: 4.3px;
+  width: 97px;
+}
+
+.body-studentstats {
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  margin-right: 4px;
+  margin-top: 55px;
+  min-height: 218px;
+  position: relative;
+  width: 241px;
+}
+
+.overlap-group-1-studentstats {
+  align-items: center;
+  background-color: var(--white-32);
+  border-radius: 14px;
+  box-shadow: 0px 4px 4px #00000040;
+  display: flex;
+  height: 54px;
+  min-width: 241px;
+  padding: 0 9px;
+  position: relative;
+}
+
+.homepage-studentstats {
+  display: flex;
+  width: 185px;
+}
+
+.icon-home-studentstats {
+  height: 40px;
+  width: 40px;
+}
+
+.homepage-1-studentstats {
+  height: 24px;
+  letter-spacing: 0;
+  margin-left: 17px;
+  margin-top: 8px;
+  width: 125.6px;
+}
+
+.groups-studentstats {
+  cursor: pointer;
+  display: flex;
+  margin-left: 15px;
+  margin-top: 39px;
+  width: 163px;
+}
+
+.groups-1-studentstats {
+  height: 45px;
+  width: 36px;
+}
+
+.groups-2-studentstats {
+  height: 24px;
+  letter-spacing: 0;
+  margin-left: 17px;
+  margin-top: 11px;
+  width: 107.83px;
+}
+
+.settings-studentstats {
+  cursor: pointer;
+  display: flex;
+  margin-left: 9px;
+  margin-top: 40px;
+  width: 155px;
+}
+
+.icon-settings-studentstats {
+  height: 40px;
+  width: 40px;
+}
+
+.settings-1-studentstats {
+  height: 24px;
+  letter-spacing: 0;
+  margin-left: 17px;
+  margin-top: 8px;
+  width: 95.98px;
+}
+</style>
